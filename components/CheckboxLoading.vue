1abfad0c389c24c9f7824e77deb6d2c042115420
Dario Garcia <dario61081@gmail.com>
2025-03-28 17:22:25 -0300

    agregado output

---
+++ components/CheckboxLoading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center animate-pulse">
+    <div class="w-4 h-4 rounded border border-gray-300 bg-gray-200"></div>
+    <div class="ml-2 h-4 w-24 bg-gray-200 rounded"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxLoading'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center p-4">
+    <div class="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900 mr-3"></div>
+    <span class="text-gray-700 font-medium">Cargando...</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+<style scoped>
+</style>
