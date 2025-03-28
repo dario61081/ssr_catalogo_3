2d53a61abf84a61f201278aefa0b921408e2e61c
Dario Garcia <dario61081@gmail.com>
2025-03-28 19:48:53 -0300

    refactoring - pagina de contacto

---
+++ layouts/simple-layout.vue
@@ -0,0 +1,10 @@
+<template>
+    <SimpleHeader />
+    <slot></slot>    
+    <Footer />
+</template>
+
+<script setup>
+import SimpleHeader from '~/components/SimpleHeader.vue'
+import Footer from '~/components/Footer.vue'
+</script>
